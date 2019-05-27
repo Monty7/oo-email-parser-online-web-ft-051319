fd036f7c21164d6647b709d4ade017a535fa8758
M Banks <montensia1@outlook.com>
2019-05-27 16:35:13 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,7 +13,7 @@ class EmailParser
     @email_arr_space = @email_addresses.split(" ")
     @emails = [@email_arr_comma, @email_addresses]
     @emails.flatten
-    
+    @emails_combined = @emails.uniq
   end
   
 end
